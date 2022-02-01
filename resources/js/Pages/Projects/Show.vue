d1e3021be45f5da72d71f16a8a566f76854f7f62
Sam Rapaport <me@samrapaport.com>
2022-02-01 11:07:20 -0800

    Basic image gallery functionality.

--- resources/js/Pages/Projects/Show.vue
+++ resources/js/Pages/Projects/Show.vue
@@ -8,7 +8,7 @@
             <img src="https://via.placeholder.com/1440x1080" alt="">
         </div>
         <div class="flex flex-wrap my-3">
-            <div class="my-3 w-full md:w-1/3">
+            <div class="my-3 w-full lg:w-1/3">
                 <span class="block my-3">
                     DEUS:<br>
                     REX-13:<br>
@@ -18,8 +18,14 @@
                     IG:<br>
                 </span>
             </div>
-            <div class="mt-3 w-full md:w-2/3">
+            <div class="mt-3 w-full lg:w-2/3">
                 {{ project.description }}
+            </div>
+        </div>
+
+        <div class="flex flex-wrap w-full -mx-2">
+            <div v-for="(image, i) in images" :key="i" class="w-1/4 p-2 cursor-pointer">
+                <img :src="image" alt="" class="glightbox" data-type="image">
             </div>
         </div>
     </div>
@@ -28,9 +34,28 @@
 </template>
 
 <script>
+import GLightbox from 'glightbox';
+
 export default {
     props: {
         project: Object,
+    },
+    data() {
+        return {
+            images: [
+                'https://via.placeholder.com/1440x1080',
+                'https://via.placeholder.com/1440x1080',
+                'https://via.placeholder.com/1440x1080',
+                'https://via.placeholder.com/1440x1080',
+                'https://via.placeholder.com/1440x1080',
+                'https://via.placeholder.com/1440x1080',
+            ],
+        }
+    },
+    mounted() {
+        const gallery = GLightbox({
+            selector: '.glightbox',
+        })
     }
 }
 </script>

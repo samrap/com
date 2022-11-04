7d5bb0f93c605e771666976f95c48190e21ed729
Sam Rapaport <me@samrapaport.com>
2022-11-03 19:47:36 -0700

    Send it 🚀

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,17 +1,8 @@
 <template>
     <nav>
         <ul class="text-center">
-            <li class="my-4">
-                <a href="https://fiveninesclo.com" target="_blank" class="inline-block py-2 font-bold text-2xl text-white uppercase">Shop</a>
-            </li>
-            <li class="my-4">
-                <Link href="/portfolio" class="inline-block py-2 font-bold text-2xl text-white uppercase">Portfolio</Link>
-            </li>
-            <li class="my-4">
-                <a href="https://mag.samrapaport.com" class="inline-block py-2 font-bold text-2xl text-white uppercase">Mag</a>
-            </li>
-            <li class="my-4">
-                <Link href="/about" class="inline-block py-2 font-bold text-2xl text-white uppercase">About</Link>
+            <li>
+                <a href="https://thatsrdd.com" class="block w-full px-4 py-4 bg-blue text-white font-bold text-xl text-center">That's <em>Really</em> Dope &rarr;</a>
             </li>
         </ul>
     </nav>
